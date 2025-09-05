34a84e338c74a2e09df167c926cf6c7cbf8784af
polianarc <polianarc934@gmail.com>
2025-09-05 01:59:16 -0300

    get and post methods

--- backend/project-back/demo/src/main/java/com/teste_pds/demo/controller/MenuController.java
+++ backend/project-back/demo/src/main/java/com/teste_pds/demo/controller/MenuController.java
@@ -1,4 +1,28 @@
 package com.teste_pds.demo.controller;
 
+import com.teste_pds.demo.model.Menu;
+import com.teste_pds.demo.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class MenuController {
+
+    @Autowired
+    private MenuRepository menuRepository;
+
+    @PostMapping("/menu")
+    Menu newMenu(@RequestBody Menu newMenu) {
+        return menuRepository.save(newMenu);
+    }
+
+    @GetMapping("/menu")
+    List<Menu> gettAllMenu() {
+        return menuRepository.findAll();
+    }
 }
